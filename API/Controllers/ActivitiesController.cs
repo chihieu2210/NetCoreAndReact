db0bb1b935bea6c8737025b8118e015ddea056d9
troylam <hieu.lam@orientsoftware.net>
2021-12-05 19:05:08 +0700

    Adding an API controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,22 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+  public class ActivitiesController : BaseApiController
+  {
+    private readonly DataContext _context;
+    public ActivitiesController(DataContext context)
+    {
+      _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+      return await (_context.Activities != null ? _context.Activities.ToListAsync() : null);
+    }
+  }
+}
