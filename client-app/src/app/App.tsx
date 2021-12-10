6e79eadcd247e91d2b43007dd48fcbaac38be1f2
troylam <hieu.lam@orientsoftware.net>
2021-12-10 17:22:49 +0700

    Folder structure in React

--- client-app/src/app/App.tsx
+++ client-app/src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./styles.css";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
