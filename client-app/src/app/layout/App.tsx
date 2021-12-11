fa68cd4e5516d9ea74c9ba32847e62de8d2d532b
troylam <hieu.lam@orientsoftware.net>
2021-12-11 11:30:27 +0700

    Adding an Activity interface

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,12 +3,13 @@ import "./styles.css";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
 import { Header, List } from "semantic-ui-react";
+import { Activity } from "../models/activity";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5271/api/activities").then((response) => {
+    axios.get<Activity[]>("http://localhost:5271/api/activities").then((response) => {
       setActivities(response.data);
     });
   }, []);
@@ -17,7 +18,7 @@ function App() {
     <div>
       <Header as="h2" icon="users" content="Activities" />
       <List>
-        {activities.map((activity: any) => {
+        {activities.map((activity) => {
           return <List.Item key={activity.id}>{activity.title}</List.Item>;
         })}
       </List>
