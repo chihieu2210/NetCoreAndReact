e52377639226f9f637e15aa09324372e3ae30246
troylam <hieu.lam@orientsoftware.net>
2021-12-11 16:41:50 +0700

    Adding some style to the nav bar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 import "semantic-ui-css/semantic.min.css";
 import axios from "axios";
-import { List } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -18,11 +19,9 @@ function App() {
   return (
     <>
       <NavBar />
-      <List>
-        {activities.map((activity) => {
-          return <List.Item key={activity.id}>{activity.title}</List.Item>;
-        })}
-      </List>
+      <Container style={{ marginTop: "6rem" }}>
+        <ActivityDashboard activities={activities} />
+      </Container>
     </>
   );
 }
