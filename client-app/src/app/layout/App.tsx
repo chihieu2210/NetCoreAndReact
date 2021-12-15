0cbd65ae231bb9d68a587d5bf9e1d678539230ae
troylam <hieu.lam@orientsoftware.net>
2021-12-15 16:40:04 +0700

    Installing React Router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,21 @@
-import { useEffect } from "react";
 import "./styles.css";
 import "semantic-ui-css/semantic.min.css";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
 import { observer } from "mobx-react-lite";
-import { useStore } from "../stores/store";
+import HomePage from "../../features/home/HomePage";
+import { Route } from "react-router-dom";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) {
-    return <LoadingComponent />;
-  }
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: "6rem" }}>
-        <ActivityDashboard />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </>
   );
