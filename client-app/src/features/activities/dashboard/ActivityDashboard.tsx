0858dcfddb26571aa06de88bf1e9215389caaa02
troylam <hieu.lam@orientsoftware.net>
2021-12-16 20:13:13 +0700

    Cleaning up unused code

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,10 +7,13 @@ import ActivityList from "./ActivityList";
 
 function ActivityDashboard() {
   const { activityStore } = useStore();
+  const { loadActivities, activityRegistry } = activityStore;
 
   useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
+    if (activityRegistry.size <= 1) {
+      loadActivities();
+    }
+  }, [activityRegistry.size, loadActivities]);
 
   if (activityStore.loadingInitial) {
     return <LoadingComponent />;
